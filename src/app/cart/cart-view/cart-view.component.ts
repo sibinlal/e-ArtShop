4d5abe6852340d3c70ca8e7ed3d2136e6739f79c
sibinlal <sbnlalpnr@gmail.com>
2024-08-03 22:29:44 +0530

    added shopping cart logic

--- src/app/cart/cart-view/cart-view.component.ts
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-cart-view',
   templateUrl: './cart-view.component.html',
   styleUrl: './cart-view.component.css'
 })
-export class CartViewComponent {
+export class CartViewComponent implements OnInit {
+
+  cartItems: Product[] = [];
+    constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.getCartItems().subscribe(data => {
+      this.cartItems = data;
+    });
+  }
+
+  
 
 }
--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartViewComponent } from './cart-view/cart-view.component';
+import {MatCardModule} from '@angular/material/card';
+import { FlexModule } from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
@@ -9,7 +14,10 @@ import { CartViewComponent } from './cart-view/cart-view.component';
     CartViewComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatListModule,
+    MatButtonModule
   ]
 })
 export class CartModule { }
